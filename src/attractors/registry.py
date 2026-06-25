d03e527976b7e92fba1bd0effc726f9cb87aac67
aymenhafeez <hafeezaymen2357@gmail.com>
2026-06-25 21:25:09 +0100

    fix: attractor config imports

--- src/attractors/registry.py
+++ src/attractors/registry.py
@@ -1,8 +1,8 @@
-from aizawa import _aizawa_attractor
-from burker_shaw import _burke_shaw_attractor
-from loop_choatic import _loop_chaotic_attractor
-from lorenz import _lorenz_attractor
-from dadras import _dadras_attractor
+from .aizawa import _aizawa_attractor
+from .burke_shaw import _burke_shaw_attractor
+from .loop_choatic import _loop_chaotic_attractor
+from .lorenz import _lorenz_attractor
+from .dadras import _dadras_attractor
 
 ATTRACTORS = {
     "Lorenz": _lorenz_attractor,
