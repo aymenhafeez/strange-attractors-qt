4eefe197de8cc8534e6fda81f666d56f3bbe6a79
aymenhafeez <hafeezaymen2357@gmail.com>
2026-08-05 16:42:23 +0100

    add: COnsoleParam set_value and slider and spinbox change methods

--- src/attractors/console/explorer.py
+++ src/attractors/console/explorer.py
@@ -2,6 +2,8 @@ from pyqtgraph.Qt import QtCore, QtWidgets
 
 
 class ConsoleParam(QtCore.QObject):
+    """Console created parameter backed by a slider and spinbox"""
+
     changed = QtCore.pyqtSignal()
 
     def __init__(self, name, value, start, end, step, parent=None):
@@ -10,7 +12,7 @@ class ConsoleParam(QtCore.QObject):
         self.start = start
         self.end = end
         self.step = step
-        self._value = value
+        self.value = value
 
         self.widget = QtWidgets.QWidget()
         layout = QtWidgets.QHBoxLayout(self.widget)
@@ -30,3 +32,30 @@ class ConsoleParam(QtCore.QObject):
         layout.addWidget(label)
         layout.addWidget(self.slider, 1)
         layout.addWidget(self.spin)
+
+        self.set_value(self.value, emit=False)
+
+    def set_value(self, value, *, emit=True):
+        value = min(self.end, max(self.start, float(value)))
+        index = round((value - self.start) / self.step)
+        snapped = self.start + index * self.step
+        self.value = snapped
+
+        with QtCore.QSignalBlocker(self.slider), QtCore.QSignalBlocker(self.spin):
+            self.slider.setValue(index)
+            self.spin.setValue(snapped)
+
+        if emit:
+            self.changed.emit()
+
+    def _on_slider_changed(self, index):
+        self.set_value(self.start + index * self.step)
+
+    def _on_spin_changed(self, value):
+        self.set_value(value)
+
+    def __float__(self):
+        return self.value
+
+    def __repr__(self):
+        return f"ConsoleParam({self.name!r}, value={self.value:g})"
