845ef234017f4f02a4b1bab266af26e42dd053da
aymenhafeez <hafeezaymen2357@gmail.com>
2026-08-05 17:13:35 +0100

    add: ConsoleExplorer class

--- src/attractors/console/explorer.py
+++ src/attractors/console/explorer.py
@@ -82,3 +82,43 @@ class ConsoleCurve:
             raise ValueError("x and y must be the same length")
 
         self.item.setData(x_array, y_array)
+
+
+class ConsoleExplorer(QtCore.QObject):
+    """Manage reactive plots and parameters created in the console"""
+
+    def __init__(self, plots, param_layout, status_callback=None, parent=None):
+        super().__init__(parent)
+        self.plots = plots
+        self.param_layout = param_layout
+        self.status_callback = status_callback
+        self._params = {}
+        self._curves = {}
+
+    def slider(self, name, value=0.0, start=0.0, end=1.0, step=0.01):
+        key = name.strip()
+        if not key:
+            raise ValueError("Slider key cannot be empty")
+
+        old = self._params.pop(key, None)
+        if old is not None:
+            self.param_layout.removeWidget(old.widget)
+            old.widget.deleteLater()
+
+        param = ConsoleParam(key, value, start, end, step, parent=self)
+        param.changed.connect(self.refresh)
+        self._params[key] = param
+        self.param_layout.addWidget(param.widget)
+
+        return param
+
+    def refresh(self):
+        for curve in list(self._curves.values()):
+            try:
+                curve.update()
+            except Exception as e:
+                self._set_status(str(e))
+
+    def _set_status(self, message):
+        if self.status_callback is not None:
+            self.status_callback(message)
